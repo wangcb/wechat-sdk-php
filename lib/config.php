5606a58c847456a08b81ad6d0cc2cc7d97fa98e8
wangcb <wangcb615@163.com>
2018-02-23 16:51:00 +0800

    Revert "Revert "微信公众号sdk""
    This reverts commit 7ccfafc8a9dc6abe256538585feb9000847569ee.

---
+++ lib/config.php
@@ -0,0 +1,17 @@
+<?php 
+return array (
+  'VerifyTicket' => '',
+  'ComponentAccessToken' => 
+  array (
+    'component_access_token' => '',
+    'expires_in' => 0,
+    'time' => 0,
+  ),
+  'PreAuthCode' => 
+  array (
+    'pre_auth_code' => '',
+    'expires_in' => 0,
+    'time' => 0,
+  ),
+); 
+?>
